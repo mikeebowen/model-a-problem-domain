c0410f8b46f120cf9408d199665891ae1c40a906
Ben Spears <bennett.spears@gmail.com>
2015-01-14 12:57:00 -0800

    add constructer for driver and car.

---
+++ car-race-model.js
@@ -0,0 +1,19 @@
+<script>
+
+  function Driver(name, driverSkill){
+    this.driverSkill = driverSkill;
+    this.name        = name;
+    this.wins        = 0;
+    this.lossses     = 0;
+  }
+
+function Car(model, speed) {
+  this.model = model;
+  this.speed = speed;
+}
+
+
+
+
+
+</script>
