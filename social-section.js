591fd4ca42339cc3f31716d4973c530890b39104
Michael Bowen <mikee.bowen@gmail.com>
2015-01-18 19:32:35 -0800

    Style front page and apply style to race page

---
+++ social-section.js
@@ -0,0 +1,14 @@
+// var socialIcons = document.getElementById(social);
+// var windowSize = window.innerWidth;
+// var cName = socialIcons.className;
+
+// if(window.innerwidthe < 1250px){
+//   cName = social-icons-small-screen;
+// }
+
+// alert(cName);
+if(window.innerwidthe < 1250px){
+function(){
+
+        document.getElementById('social').className = fSetClass(social-icons-small);
+};
